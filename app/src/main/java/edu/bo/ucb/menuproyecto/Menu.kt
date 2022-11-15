05cce340c97b28b9150792347bbc6e086c7c2d29
Sergio <sergio.salinas.b@ucb.edu.bo>
2022-11-15 19:28:14 -0400

    menu1

--- app/src/main/java/edu/bo/ucb/menuproyecto/Menu.kt
+++ app/src/main/java/edu/bo/ucb/menuproyecto/Menu.kt
@@ -1,5 +1,6 @@
 package edu.bo.ucb.menuproyecto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -21,10 +22,26 @@ class Menu : AppCompatActivity() {
             R.drawable.galeria,
             R.drawable.horario,
         )
+        val list2= arrayListOf<String>(
+            getString(R.string.Configuraciones),
+            getString(R.string.Alarmas),
+            getString(R.string.Cámara),
+            getString(R.string.Contactos),
+            getString(R.string.Galeria),
+            getString(R.string.Horario)
+        )
+        val list3= arrayListOf<Intent>(
+            Intent(this, MainActivity::class.java) ,
+            Intent(this, MainActivity::class.java),
+            Intent(this, MainActivity::class.java),
+            Intent(this, MainActivity::class.java),
+            Intent(this, MainActivity::class.java),
+            Intent(this, prueba::class.java),
+        )
         val gridLayout = GridLayoutManager(this,2)
         gridLayout.orientation = GridLayoutManager.VERTICAL
         rvButtons.layoutManager = gridLayout
-        rvButtons.adapter = MenuAdapter(list, applicationContext)
+        rvButtons.adapter = MenuAdapter(list,list2,list3, applicationContext)
 
     }
 }
--- app/src/main/java/edu/bo/ucb/menuproyecto/MenuAdapter.kt
+++ app/src/main/java/edu/bo/ucb/menuproyecto/MenuAdapter.kt
@@ -1,14 +1,15 @@
 package edu.bo.ucb.menuproyecto
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-
-class MenuAdapter(val list: ArrayList<Int>, applicationContext: Context?) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+//val list3: ArrayList<Intent>,
+class MenuAdapter(val list: ArrayList<Int>,val list2: ArrayList<String>,val list3: ArrayList<Intent>,val applicationContext: Context?) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.menu_row, parent, false)
         return UserListViewHolder(view)
@@ -21,21 +22,14 @@ class MenuAdapter(val list: ArrayList<Int>, applicationContext: Context?) : Recy
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val item = list.get(position)
-        val list2= arrayListOf<String>(
-            "Configuraciones",
-            "Alarmas",
-            "Cámara",
-            "Contactos",
-            "Galería",
-            "Horarios"
-
-        )
         val item2= list2.get(position)
+        val item3= list3.get(position)
         holder.itemView.findViewById<ImageView>(R.id.rv_option1).setImageResource(item)
         holder.itemView.findViewById<TextView>(R.id.textView1).text= item2
-        //val imagen= holder.itemView.findViewById<ImageView>(R.id.rv_option1)
-        //imagen.setImageResource(item)
-        //imagen.setOnClickListener()
+        holder.itemView.setOnClickListener{
+            item3.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            applicationContext?.startActivity(item3) }
+
     }
 
     override fun getItemCount(): Int {
